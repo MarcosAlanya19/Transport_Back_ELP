07ffff626817293c80f994a71ed56a4c3efd976c
Marcos <asesoralanya19@gmail.com>
2024-11-28 10:27:21 -0500

    feat: create proyect ELP

---
+++ src/main/java/com/transport/management/utils/abtractBase/BaseRepository.java
@@ -0,0 +1,7 @@
+package com.transport.management.utils.abtractBase;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepository<T> extends CrudRepository<T, Long> {}
