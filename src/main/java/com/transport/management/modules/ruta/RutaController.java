8885750e0d760b7a09d8c28ed09883d87bebcfed
jrtv2203 <torresvivancojunior35@gmail.com>
2025-01-17 10:57:03 -0500

    SEM15JUNIORTORRES: Implementación lógica ruta

--- src/main/java/com/transport/management/modules/ruta/RutaController.java
+++ src/main/java/com/transport/management/modules/ruta/RutaController.java
@@ -3,7 +3,9 @@ package com.transport.management.modules.ruta;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,29 +13,45 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.transport.management.modules.ruta.request.RutaRequest;
+
 @RestController
 @RequestMapping("/rutas")
+@CrossOrigin(origins = "*")
 public class RutaController {
   @Autowired
   RutaService rutaService;
 
   @GetMapping
-  public List<RutaEntity> findAll() {
-    return rutaService.findAll();
-  }
-
-  @GetMapping("/{id}")
-  public RutaEntity findById(@PathVariable Long id) {
-    return rutaService.findById(id);
+  public ResponseEntity<List<RutaEntity>> findAll() {
+    List<RutaEntity> rutaEntities = rutaService.findAll();
+    return ResponseEntity.status(HttpStatus.OK).body(rutaEntities);
   }
 
   @PostMapping
-  public RutaEntity save(@RequestBody RutaEntity ruta) {
-    return rutaService.save(ruta);
+  public ResponseEntity<RutaEntity> save(@RequestBody RutaRequest rutaRequest) {
+    RutaEntity ruta = new RutaEntity();
+    ruta.setOrigen(rutaRequest.getOrigen());
+    ruta.setDestino(rutaRequest.getDestino());
+    ruta.setDistancia(rutaRequest.getDistancia());
+    ruta.setPrecioBase(rutaRequest.getPrecioBase());
+
+    RutaEntity savedRuta = rutaService.save(ruta);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(savedRuta);
   }
 
-  @DeleteMapping("/{id}")
-  public void deleteById(@PathVariable Long id) {
+  @PostMapping(value = "/{id}/delete")
+  public ResponseEntity<Void> deleteRuta(@PathVariable Long id) {
     rutaService.deleteById(id);
+    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+  }
+
+  @PostMapping(value = "/{id}/update")
+  public ResponseEntity<RutaEntity> updateRuta(
+      @PathVariable Long id,
+      @RequestBody RutaRequest rutaRequest) {
+    RutaEntity updatedRuta = rutaService.update(id, rutaRequest);
+    return ResponseEntity.status(HttpStatus.OK).body(updatedRuta);
   }
 }
--- src/main/java/com/transport/management/modules/ruta/RutaRepository.java
+++ src/main/java/com/transport/management/modules/ruta/RutaRepository.java
@@ -1,5 +1,8 @@
 package com.transport.management.modules.ruta;
 
+import org.springframework.stereotype.Repository;
+
 import com.transport.management.utils.abtractBase.BaseRepository;
 
+@Repository
 public interface RutaRepository extends BaseRepository<RutaEntity> {}
