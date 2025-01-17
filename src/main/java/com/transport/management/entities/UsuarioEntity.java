287a86db2f50ec1e1a489f8af6f4c45e693e4997
Jeremy Castillo <josstinpretell3@gmail.com>
2025-01-16 23:01:34 -0500

    SEM15JEREMYCASTILLO:ImplementaciondeConfiguracionesCorrections

--- src/main/java/com/transport/management/entities/UsuarioEntity.java
+++ src/main/java/com/transport/management/entities/UsuarioEntity.java
@@ -8,13 +8,19 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Table(name = "usuario")
 @Getter
 @Setter
 @NoArgsConstructor
-public class UsuarioEntity extends BaseEntity {
+public class UsuarioEntity extends BaseEntity implements UserDetails {
   @Column(nullable = false, length = 100)
   private String nombre;
 
@@ -26,4 +32,19 @@ public class UsuarioEntity extends BaseEntity {
 
   @Column(nullable = false, length = 50)
   private String rol;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of();
+  }
+
+  @Override
+  public String getPassword() {
+    return "";
+  }
+
+  @Override
+  public String getUsername() {
+    return "";
+  }
 }
--- src/main/java/com/transport/management/repositories/UsuarioRepository.java
+++ src/main/java/com/transport/management/repositories/UsuarioRepository.java
@@ -2,6 +2,11 @@ package com.transport.management.repositories;
 
 import com.transport.management.entities.UsuarioEntity;
 import com.transport.management.utils.abtractBase.BaseRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface UsuarioRepository extends BaseRepository<UsuarioEntity> {
+    Optional<UsuarioEntity> findByEmail(String email);
 }
