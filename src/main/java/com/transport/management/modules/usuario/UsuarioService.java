21200045616a5ac2857bbd242dc3a356a9041152
lewis <luisangelcalleallcca@gmail.com>
2025-01-16 23:45:50 -0500

    SEM15LUISCALLE: Eliminacion de archivos innecesarios

--- src/main/java/com/transport/management/modules/usuario/UsuarioService.java
+++
@@ -1,9 +0,0 @@
-package com.transport.management.modules.usuario;
-
-import org.springframework.stereotype.Service;
-
-import com.transport.management.utils.abtractBase.BaseService;
-
-@Service
-public class UsuarioService extends BaseService<UsuarioEntity> {
-}
