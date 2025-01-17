eabd5d921eff0a28f526eb387a7b861587d3307f
Jeremy Castillo <josstinpretell3@gmail.com>
2025-01-16 23:02:36 -0500

    SEM15JEREMYCASTILLO:ModificacionesClasesAbstractas

--- src/main/java/com/transport/management/utils/abtractBase/BaseEntity.java
+++ src/main/java/com/transport/management/utils/abtractBase/BaseEntity.java
@@ -15,31 +15,38 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   protected Long id;
 
   @Column(updatable = false, name = "created_at")
-  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  @CreatedDate
   protected Date creadoEn;
 
   @Column(name = "updated_at")
-  @DateTimeFormat(pattern = "yyyy-MM-dd")
+  @LastModifiedDate
   protected Date actualizadoEn;
 
-  @PrePersist
-  protected void onCreate() {
-    this.creadoEn = new Date();
-    this.actualizadoEn = new Date();
-  }
-
-  @PreUpdate
-  protected void onUpdate() {
-    this.actualizadoEn = new Date();
-  }
 }
